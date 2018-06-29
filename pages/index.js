4056ad190ab8b0c9572f9e8cea1fd4020f7de3d8
Stephen Sauceda <contact@stephensauceda.com>
2018-06-28 22:43:18 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import Head from 'next/head'
+import clientCredentials from '../credentials/client'
+
+export default class Index extends Component {
+  static async getInitialProps({ req, query }) {
+    const snap = await req.firebaseServer
+      .database()
+      .ref('candaddyplay')
+      .once('value')
+    const candaddyplay = snap && snap.val()
+    return { candaddyplay }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      candaddyplay: this.props.candaddyplay
+    }
+
+    this.addDbListener = this.addDbListener.bind(this)
+  }
+
+  componentDidMount() {
+    firebase.initializeApp(clientCredentials)
+
+    this.addDbListener()
+  }
+
+  async addDbListener() {
+    firebase
+      .database()
+      .ref('candaddyplay')
+      .on('value', snap => {
+        const candaddyplay = snap.val()
+        if (candaddyplay) this.setState({ candaddyplay })
+      })
+  }
+
+  render() {
+    const { candaddyplay } = this.state
+
+    return (
+      <div className={`wrapper wrapper--${candaddyplay}`}>
+        <Head>
+          <title>Can Daddy Play Minecraft</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
+          />
+        </Head>
+        <div>
+          <h1>Can Daddy play Minecraft?</h1>
+          <h2 className="blink">{candaddyplay}</h2>
+        </div>
+        <style jsx global>{`
+          html, body, .wrapper, #__next { height: 100%; }
+          body { font-family: sans-serif; text-align: center; }
+          .wrapper { display: flex; justify-content: center; align-items: center; }
+          .wrapper--yes { background-color: #65AC3C; }
+          .wrapper--no { background-color: #A30A17; }
+          .wrapper--yes .blink {
+            animation: blink-animation 1s steps(5, start) infinite;
+            -webkit-animation: blink-animation 1s steps(5, start) infinite;
+          }
+          @keyframes blink-animation {
+            to {
+              visibility: hidden;
+            }
+          }
+          @-webkit-keyframes blink-animation {
+            to {
+              visibility: hidden;
+            }
+          }
+        `}
+        </style>
+        <style jsx>{`
+            h1 { color: white }
+            h2 { color: white; text-transform: uppercase; }
+          `}
+        </style>
+      </div>
+    )
+  }
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const next = require('next')
+const admin = require('firebase-admin')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(require('./credentials/server')),
+  databaseURL: 'https://can-daddy-play-minecraft.firebaseio.com' // TODO database URL goes here
+}, 'server')
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    server.use((req, res, next) => {
+      req.firebaseServer = firebase
+      next()
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
