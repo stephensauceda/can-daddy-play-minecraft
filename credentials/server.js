5667abcdfd91a1b8cd0df5daeb851025a5f36a84
Stephen Sauceda <contact@stephensauceda.com>
2018-06-30 15:58:29 -0400

    try parsing key in the app

--- credentials/server.js
+++ credentials/server.js
@@ -2,5 +2,5 @@
 module.exports = {
   projectID: process.env.FB_PROJECT_ID,
   client_email: process.env.FB_CLIENT_EMAIL,
-  private_key: process.env.FB_PRIVATE_KEY
+  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n')
 }
